671e96e889888b06eec7fff66217eb9cd52e7255
kupets <12082003Dima>
2024-07-21 15:53:48 +0300

    feat: add init sequelize in app.js

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const db = require('./db/model');
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+db.sequelize.sync().then(() => {
+    console.log('Database connected');
+});
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
