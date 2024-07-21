1ea738c4d68d8e116faa77c8fd2ce4d2643ba29f
kupets <12082003Dima>
2024-07-21 15:51:38 +0300

    chore: reformat sequlize.js

--- db/connection/sequelize.js
+++ db/connection/sequelize.js
@@ -1,4 +1,4 @@
-const {Sequlize} = require('sequelize');
+const Sequlize = require('sequelize');
 
 const sequelize = new Sequlize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
